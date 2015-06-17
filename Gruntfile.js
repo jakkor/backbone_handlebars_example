c0d0b65aeb9482c2bd60a65e45c80b59b5e7eede
Jakub Korupczynski <jakub.korupczynski@gmail.com>
2015-06-17 18:14:36 +0100

    Added some basic http-server to run the app

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-broccoli');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-http-server');
 
     grunt.initConfig({
       broccoli: {
@@ -17,8 +18,26 @@ module.exports = function(grunt) {
       clean: {
         dist: ['./tmp']
       },
+      'http-server': {
+        dev: {
+            root: 'dist',
+            port: 9000,
+            host: "0.0.0.0",
+            showDir : true,
+            autoIndex: true,
+            // server default file extension
+            ext: "html",
+            // run in parallel with other tasks
+            runInBackground: false,
+
+            // specify a logger function. By default the requests are
+            // sent to stdout.
+            logFn: function(req, res, error) { }
+        }
+    }
     });
 
     grunt.registerTask('dist', ['clean:dist', 'broccoli:dist:build']);
     grunt.registerTask('default', ['clean:dist', 'broccoli:dev:build']);
+    grunt.registerTask('run', ['http-server', 'clean:dist', 'broccoli:dev:build']);
 };
